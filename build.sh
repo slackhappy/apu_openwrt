b04511224b3228bb773bc7be626061de63b7a16b
John Gallagher <jmg2016@gmail.com>
2020-01-23 10:38:33 -0500

    configurable version, bump to 18.06.6

--- build.sh
+++ build.sh
@@ -2,14 +2,20 @@
 
 set -eo pipefail
 
+VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+  VERSION="18.06.2"
+fi
+
 # from https://openwrt.org/docs/guide-user/additional-software/imagebuilder
 DEPS="build-essential libncurses5-dev libncursesw5-dev zlib1g-dev gawk git gettext libssl-dev xsltproc wget unzip python"
 
-VERSION="18.06.2"
+
+EXTRA_IMAGE_NAME="apu2-ath10k-qca988x"
 
 PACKAGES="adblock ath10k-firmware-qca988x block-mount ca-bundle collectd collectd-mod-sensors flashrom fstools hostapd kmod-ath10k kmod-crypto-hw-ccp kmod-fs-vfat kmod-gpio-button-hotplug kmod-gpio-nct5104d kmod-leds-gpio kmod-pcspkr kmod-rt2800-lib kmod-rt2800-usb kmod-rt2x00-lib kmod-rt2x00-usb kmod-sound-core kmod-sp5100_tco kmod-usb-ohci kmod-usb-storage kmod-usb-storage-uas kmod-usb2 kmod-usb3 luci luci-app-statistics rt2800-usb-firmware tcpdump sysfsutils usbutils wget"
 
-EXTRA_IMAGE_NAME="apu2_ath10k_qca988x"
 
 BUILDER_NAME="openwrt-imagebuilder-${VERSION}-x86-64.Linux-x86_64"
 
@@ -29,4 +35,4 @@ cd "${BUILDER_NAME}"
 make image PACKAGES="${PACKAGES}" EXTRA_IMAGE_NAME="${EXTRA_IMAGE_NAME}"
 
 echo "***** OUTPUT *****"
-ls bin/targets/x86/64/
+ls bin/targets/x86/64/bin/targets/x86/64/openwrt-${VERSION}-${EXTRA_IMAGE_NAME}-x86-64-combined-ext4.img.gz
